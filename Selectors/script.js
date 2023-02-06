0c8774d6226cc8f90a16abc48b13b60566fa1b08
islam altayeb <eslameltaieb74@gmail.com>
2023-02-06 19:03:06 +0100

    jQuery-Crash Course

---
+++ Selectors/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// $('.heading-2').hide();
+// $('#heading-1').hide();
+$('p span').css('color','red');
+$('ul#list li:first').css('color','blue');
+$('ul#list li:even').css('background-color','gray');
+$(':button').css('background-color','red');
+$('[href]').css('text-decoration','none');
+$('a[href ="http://www.youtube.com/"]').css('color','green');
